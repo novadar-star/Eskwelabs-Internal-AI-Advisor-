62d564c58bf21d1eb1a069bf1fef449eedf7a519
novadar-star <jolynekujoismywife06@gmail.com>
2026-06-14 21:56:03 +0800

    removed direct themeproviderprops

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -14,6 +14,7 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import type { ComponentProps } from "react";
+
 type ThemeProviderProps = ComponentProps<typeof NextThemesProvider>;
 
 export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
