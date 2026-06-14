3bf3cf4a1875dda6d0f64d5a53b7d6ed69195d3d
novadar-star <jolynekujoismywife06@gmail.com>
2026-06-14 21:36:18 +0800

    fixed nex themes export

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -13,7 +13,8 @@
  */
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import type { ThemeProviderProps } from "next-themes";
+import type { ComponentProps } from "react";
+type ThemeProviderProps = ComponentProps<typeof NextThemesProvider>;
 
 export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
